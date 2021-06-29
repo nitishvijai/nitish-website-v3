ebab415877868683b9dc8cf97c813c8061413b1b
nitishv <nitishv@umich.edu>
2021-06-28 23:03:45 -0400

    Update about me component with current text
    Made some minor adjustments to the text. Need to add status as well.

--- src/app/components/about-me/about-me.component.ts
+++ src/app/components/about-me/about-me.component.ts
@@ -1,15 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-about-me',
   templateUrl: './about-me.component.html',
   styleUrls: ['./about-me.component.css']
 })
-export class AboutMeComponent implements OnInit {
+export class AboutMeComponent implements OnInit, OnDestroy {
 
   constructor() { }
 
   ngOnInit() {
+    (document.querySelector('#aboutme') as HTMLElement).style.borderBottom = "2px solid black";
+  }
+
+  ngOnDestroy() {
+    (document.querySelector('#aboutme') as HTMLElement).style.borderBottom = "";
   }
 
 }
