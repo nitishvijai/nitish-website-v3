886ee473fdd9eaf01f3099a78948a7e21937a38a
nitishv <nitishv@umich.edu>
2021-06-29 14:07:57 -0400

    Add more projects and update project layout page
    The page is still in a rough state, need to create different project
    card components and improve the design.

--- src/app/components/projects/projects.component.ts
+++ src/app/components/projects/projects.component.ts
@@ -11,7 +11,9 @@ export class ProjectsComponent implements OnInit, OnDestroy {
   constructor() { }
 
   projects: Project[] = [
-    { name: 'Test Project', description: 'Placeholder for actual project', skills: 'HTML, CSS', startdate: 'June 2021', enddate: 'Present', type: 'personal' }
+    { name: 'Eventempo', description: 'Built a UWP countdown timer app for Windows 10 devices. Some features include support for multiple event countdown timers, ease of use, event autosave, colorful customization, and notification alerts. Worked on this app during winter break of my freshman year in college, and released incremental updates to the app in the following months.', skills: 'C# .NET (backend), XAML (frontend), Visual Studio', startdate: 'December 2019', enddate: 'February 2020', type: 'personal', github: 'https://github.com/nitishvijai/Eventempo' },
+    { name: 'Store Scraper', description: 'Developed a web scraper in Python to scrape the top search result from three retailers and return the lowest price. Utilized BeautifulSoup to scrape the retailers and Selenium to emulate a real web browser and bypass dynamic JavaScript changes. In addition, created and published a tutorial on Enlight, an open-source educational platform that helps one learn to code by building cool projects.', skills: 'Python, BeautifulSoup, Selenium, VSCode', startdate: 'January 2020', enddate: 'April 2020', type: 'personal', github: 'https://github.com/nitishvijai/StoreScraper' },
+    { name: 'Personal Website V2.0', description: 'Designed and developed a personal website to show my portfolio of skills and projects. In addition, this website was designed from scratch to replace my previous GitHub Pages site, breaking free from template design constraints and gaining a more thorough understanding of HTML, CSS, and JS.', skills: 'HTML5, CSS3, JavaScript, jQuery, VSCode', startdate: 'April 2020', enddate: 'June 2020', type: 'personal', github: 'https://github.com/nitishvijai/nitish-website-v2' }
   ];
 
   personal: boolean = true;
