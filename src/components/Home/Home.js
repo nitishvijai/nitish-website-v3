aaebf3e94f6e00d3a4c6032b781959ea08f3034a
nitishvijai <nitishv@umich.edu>
2023-01-27 23:26:54 -0500

    Added portrait mode for home page

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,38 +1,46 @@
-import React from 'react';
+import { React, useRef } from 'react';
 import styles from './Home.module.css';
 import Navbar from '../Navbar/Navbar';
 import Footer from '../Footer/Footer';
+import useMediaQuery from '@mui/material/useMediaQuery';
 
 const Home = () => {
+  const mobilePortrait = useMediaQuery('(max-width:1024px)');
+  const gradient = useRef(null);
+
   let moveGradient = (e) => {
-    let btn = document.querySelector('.gradient, a');
     let x = e.clientX - 0;
     let y = e.clientY - 0;
-    btn.style.setProperty('--x', x + 'px');
-    btn.style.setProperty('--y', y + 'px');
+    if (!mobilePortrait) {
+      gradient.current.style.setProperty('--x', x + 'px');
+      gradient.current.style.setProperty('--y', y + 'px');
+    } else {
+      gradient.current.style.setProperty('--x', '0px');
+      gradient.current.style.setProperty('--y', '0px');
+    }
   }
 
   return (
-    <div className="Home gradient" onMouseMove={(e) => moveGradient(e)}>
+    <div className={mobilePortrait ? styles.gradient_mobile : styles.gradient} onMouseMove={(e) => moveGradient(e)} ref={gradient}>
       <Navbar />
-      <div id={styles.main}>
+      <div id={mobilePortrait ? styles.main_mobile : styles.main}>
         <p className={styles.message}>Hello and welcome to my site! You're in the right place if you're looking for everything I've done in the cool world of tech -- thanks for visiting!</p>
         <p className={styles.message}>To get started, please click on the links above to learn more about me.</p>
       </div>
-      <div id={styles.tldr}>
+      <div id={mobilePortrait ? styles.tldr_mobile : styles.tldr}>
         <h2>tl;dr:</h2>
-        <div id={styles.left}>
-          <img src="/Nitish_2022.jpg" className={styles.headshot} />
+        <div id={mobilePortrait ? styles.left_mobile : styles.left}>
+          <img src="/Nitish_2022.jpg" className={mobilePortrait ? styles.headshot_mobile : styles.headshot} />
         </div>
         <div id={styles.right}>
-          <h2 className={styles.fact} id={styles.tldrHeading}>&nbsp;&nbsp;Nitish Vijai</h2>
-          <p  className={styles.fact}>📚 B.S.E. Computer Science and Data Science @ University of Michigan</p>
-          <p  className={styles.fact}>🖥️ Incoming Associate Software Engineer @ Capital One</p>
-          <p  className={styles.fact}>⚒️ Currently working on this website</p>
+          <h2 className={mobilePortrait ? styles.fact_mobile : styles.fact} id={styles.tldrHeading}>&nbsp;&nbsp;Nitish Vijai</h2>
+          <p  className={mobilePortrait ? styles.fact_mobile : styles.fact}>〽️ B.S.E. Computer Science and Data Science @ University of Michigan</p>
+          <p  className={mobilePortrait ? styles.fact_mobile : styles.fact}>🖥️ Incoming Associate Software Engineer @ Capital One</p>
+          <p  className={mobilePortrait ? styles.fact_mobile : styles.fact}>⚒️ Currently working on this website</p>
         </div>
-        
       </div>
       <Footer />
+      
     </div>
   );
 };
